3e28201ff0a930edf022486c96b75c4d8b18c8e8
Kolbe Kegel <kolbe@kolbekegel.com>
2022-09-13 16:50:42 -0700

    added some usage info etc. to route_manip.bash

--- kolbe-eks-scripts/route_manip.bash
+++ kolbe-eks-scripts/route_manip.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# source this file, then you can use the isolate_region and restore_region shell commands
+# set cluster_name in the desc_routes function according to your own scheme
+# set the cidrs for your regions' VPCs in the "cidrs" associative array
+# set the VPC peering connection IDs in the routes associative array
+
 desc_routes(){
     local region=$1
     local cluster_name="test-$region"
@@ -32,8 +37,24 @@ del_route(){
 }
 
 isolate_region(){
+    usage="first argument should be the region whose routing tables you want to manipulate
+other (optional) arguments should be the regions you want to remove from the first region's routing tables
+if only one argument is given, remove all the routes to peers from its routing tables"
+    if ! [[ $1 ]]
+    then
+        echo >&2 "$usage"
+        return 1
+    fi
+
     local region=$1
     shift
+
+    if ! [[ "${cidrs[$region]}" ]]
+    then
+        echo >&2 "region $region is not supported"
+        return 1
+    fi
+
     declare -a peers=("$@")
     if ! (( ${#peers[@]} ))
     then
@@ -57,9 +78,23 @@ isolate_region(){
 }
 
 restore_region(){
-    #set -x
+    usage="first argument should be the region whose routing tables you want to restore
+the function will just try to create all routes for all peer regions across all Private subnets"
+    if ! [[ $1 ]]
+    then
+        echo >&2 "$usage"
+        return 1
+    fi
+
     local region=$1
     shift
+
+    if ! [[ "${cidrs[$region]}" ]]
+    then
+        echo >&2 "region $region is not supported"
+        return 1
+    fi
+
     declare -a peers
     for peer in "${!cidrs[@]}"
     do
@@ -76,56 +111,4 @@ restore_region(){
             aws --region="$region" ec2 create-route --route-table-id "$table" --destination-cidr-block "${cidrs["$peer"]}" --vpc-peering-connection-id "${routes["$region.$peer"]}"
         done
     done
-    #set +x
 }
-
-
-
-#
-## jq -r '.RouteTables[] | select(.Routes | any(.DestinationCidrBlock=="10.100.0.0/16" or .DestinationCidrBlock=="10.102.0.0/16")) | .RouteTableId' |
-#isolate_east2(){
-#    local region=us-east-2
-#    desc_routes "$region" |
-#    while read -r table
-#    do
-#        echo "$table"
-#        del_route "$region" "$table" "10.100.0.0/16" "10.102.0.0/16"
-#    done
-#}
-#
-#restore_east2(){
-#    set -x
-#    local region=us-east-2
-#    local table
-#    desc_routes "$region" |
-#    while read -r table 
-#    do
-#        for peer in us-east-1 us-west-2
-#        do
-#            aws --region="$region" ec2 create-route --route-table-id "$table" --destination-cidr-block "${cidrs["$peer"]}" --vpc-peering-connection-id "${routes["$region.$peer"]}"
-#        done
-#    done
-#    set +x
-#}
-#
-##    jq -r '.RouteTables[] | select(.Routes | any(.DestinationCidrBlock=="10.101.0.0/16")) | .RouteTableId' |
-#isolate_east1_from_east2(){
-#    local region=us-east-1
-#    local table
-#    desc_routes "$region" |
-#    while read -r table
-#    do
-#        echo "$table"
-#        del_route "$region" "$table" "10.101.0.0/16"
-#    done
-#}
-#
-##    jq -r '.RouteTables[] | select(.Tags | any(.Key=="aws:cloudformation:logical-id" and (.Value|startswith("Private")))) | .RouteTableId' |
-#restore_east1_from_east2(){
-#    local region=us-east-1
-#    desc_routes "$region" |
-#    while read -r table
-#    do
-#        aws --region="$region" ec2 create-route --route-table-id "$table" --destination-cidr-block "10.101.0.0/16" --vpc-peering-connection-id pcx-098f1e7ddfbe8f9c3
-#    done
-#}
